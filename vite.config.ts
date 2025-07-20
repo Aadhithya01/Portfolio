6d60f0bf52c4016fdf8df7b307fd650f7353e72c
Aadhithya R <aadhithyaraja180@gmail.com>
2025-07-20 17:52:31 +0000

    vite.confog.ts updated added base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Portfolio/', // 👈 ADD THIS LINE
   build: {
     outDir: 'docs',
     emptyOutDir: true,
